d78accb340eb90bc898c96e54e8f352d50ae7e98
Samuelfjr <devsamuelfjr@gmail.com>
2024-12-24 14:21:01 -0300

    05 | feat: Create function areYouPlayingBanjo

--- 05.AreYouPlayingBanjo/index.js
+++ 05.AreYouPlayingBanjo/index.js
@@ -10,3 +10,11 @@ name + " plays banjo"
 name + " does not play banjo"
 Names given are always valid strings.
 */
+
+const areYouPlayingBanjo = (name) => {
+  if (name.charAt(0).toLowerCase() == "r") {
+    return `${name} plays banjo`;
+  } else {
+    return `${name} does not play banjo`;
+  }
+};
