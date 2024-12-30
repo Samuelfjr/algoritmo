cd80319629feef2a44f4999a0499e49719f70e05
Samuelfjr <devsamuelfjr@gmail.com>
2024-12-30 20:14:27 -0300

    08 | feat: Create function boolToWord

--- 08.BoolToWord/index.js
+++ 08.BoolToWord/index.js
@@ -1,3 +1,8 @@
 /**
 Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
  */
+
+function boolToWord(bool) {
+  const test = bool ? "Yes" : "No";
+  return test;
+}
