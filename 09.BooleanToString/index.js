226265827537b32b0c2438abe526fb1e6561c17c
Samuelfjr <devsamuelfjr@gmail.com>
2024-12-31 09:40:44 -0300

    09 | feat: Create function booleanToString

--- 09.BooleanToString/index.js
+++ 09.BooleanToString/index.js
@@ -1,5 +1,13 @@
-/**
+/*
 Implement a function which convert the given boolean value into its string representation.
 
 Note: Only valid inputs will be given.
- */
+*/
+
+function booleanToString(b) {
+  const boolTrue = "true";
+  const boolFalse = "false";
+  const booleanToString = b ? boolTrue : boolFalse;
+
+  return booleanToString;
+}
