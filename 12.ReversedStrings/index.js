886f484f60642e4a65a8928ea7f9055afbb4590a
Samuelfjr <devsamuelfjr@gmail.com>
2025-01-02 18:39:16 -0300

    12 | feat Create function reversedStrings

--- 12.ReversedStrings/index.js
+++ 12.ReversedStrings/index.js
@@ -6,3 +6,12 @@ Complete the solution so that it reverses the string passed into it.
 'world'  =>  'dlrow'
 'word'   =>  'drow
 */
+
+const reversedStrings = (str) => {
+  let newStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
+};
